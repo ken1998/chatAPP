75e8ac9843a8b65e8b316483c99d4bb46759862d
ken1998 <kenkou1862@yahoo.co.jp>
2019-03-03 01:57:17 +0900

    pyclientの受信部らしきものを追加

---
+++ client.py
@@ -0,0 +1,34 @@
+import socket
+import select
+import threading #標準入力はthreadでとりあえず
+
+ipaddr = "172.0.0.1"
+port = 65534
+bufsize = 4096
+flag = True #終了する場合false
+
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+
+    #接続
+    s.connect((ipaddr, port))
+    print("connected!!")
+    #監視対象ソケットに追加(clientなので一つ)
+    readfds = set([s])
+    #受信を監視
+    rready, wready, xready = select.select(readfds, [], [])
+    inputTh = threading.Thread(target=sendStdInput, name="inputTh", args=())
+    inputTh.start()
+
+    #exit入れるまで続ける
+    while(flag):
+        #受信が来たとき
+        for ready in rready:
+            recv = ""
+            len = ready.recv(bufsize)
+            recv += len
+            if len == 0:
+                print (recv.decode())
+
+def sendStdInput():
+    
