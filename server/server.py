6f01e30e606510ceded0e6e5965114abb24e6503
ken1998 <kenkou1862@yahoo.co.jp>
2019-03-08 01:44:25 +0900

    add server.py

---
+++ server/server.py
@@ -0,0 +1,20 @@
+import socket
+import threading
+import os
+
+class TcpChatServer:
+    ipaddr = "172.0.0.1"
+    port = 65000
+    process_list = []
+    
+    
+    
+    def recv_chat():
+        hoge
+
+    def send_message():
+        hoge
+
+if __name__ == "__main__":
+    tcpchatserver = TcpChatServer()
+
