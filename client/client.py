b394ed39f3d318fbbfbe151610c34c73ef7bd1f3
kKashiwagi <kenkou1862@yahoo.co.jp>
2019-03-14 19:01:23 +0900

    edit:client server

--- client/client.py
+++ client/client.py
@@ -10,10 +10,11 @@ flag = True  # 終了する場合false
 
 def send_std_input():
     input_str = input()
+    print(input_str)
     if input_str == "exit":
         exit()
     else:
-        s.send(input_str)
+        s.send(input_str.encode('utf-16'))
 
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -21,18 +22,18 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     # 接続
     s.connect((ipaddr, port))
     print("connected!!")
-    #監視対象ソケットに追加(clientなので一つ)
-    readfds = set([s])
-    inputTh = threading.Thread(target=send_std_input, name="inputTh", args=())
+    # 監視対象ソケットに追加(clientなので一つ)
+    readfds = (s, )
+    inputTh = threading.Thread(target=send_std_input())
     inputTh.start()
 
-    #exit入れるまで続ける
+    # exit入れるまで続ける
     while flag:
-        #ソケットを監視
+        # ソケットを監視
         rready, wready, xready = select.select(readfds, [], [])
-        #受信が来たとき
+        # 受信が来たとき
         for ready in rready:
-            recv = ""
+            recv = B""
             while True:
                 len = ready.recv(bufsize)
                 recv += len
